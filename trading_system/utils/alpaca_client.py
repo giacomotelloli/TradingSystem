07cecdc4fdb97029d7cca0bf1c10969d0ee1a438
Giacomo Telloli <tell.giacomo@gmail.com>
2025-04-25 01:11:37 +0200

    aggiunto il modulo di download

--- trading_system/utils/alpaca_client.py
+++ trading_system/utils/alpaca_client.py
@@ -1,7 +1,9 @@
 import os
+from dotenv import load_dotenv
 import alpaca_trade_api as tradeapi
 from alpaca.data.historical import CryptoHistoricalDataClient
 
+load_dotenv()
 
 def get_alpaca_client():
     env = os.getenv("ENVIRONMENT", "paper").lower()
@@ -21,5 +23,14 @@ def get_alpaca_client():
 
     return tradeapi.REST(key, secret, base_url)
 
-def get_alpaca_crypto_client():
-    return CryptoHistoricalDataClient(API_KEY, SECRET_KEY)
+def get_crypto_data_client():
+    """
+    Returns a client to query historical crypto price data (bars, trades, etc.).
+    """
+    api_key = os.getenv("PAPER_API_KEY_ID")
+    api_secret = os.getenv("PAPER_API_SECRET_KEY")
+
+    if not api_key or not api_secret:
+        raise ValueError("Missing PAPER_API_KEY_ID or PAPER_API_SECRET_KEY in .env")
+
+    return CryptoHistoricalDataClient(api_key=api_key, secret_key=api_secret)
